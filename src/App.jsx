7f3539dd6821a0ea9d2bbc591011095a941495c6
Lykaios-SD <58195681+Lykaios-SD@users.noreply.github.com>
2023-04-11 18:06:49 +0200

    Agrega configuracion inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from "react-router-dom"
+import NavBar from "./components/Navbar.jsx"
+import { PrivateRoutes } from "./components/PrivateRoutes.jsx"
+
+// Views
+import Index from "./views/index.jsx"
+import Login from "./views/login.jsx"
+import Todos from "./views/todos.jsx"
 
 function App() {
   
-
   return (
     <>
+    <NavBar/>
     <Routes>
-      <Route path="/" element= {} />
+      <Route path="/" element= {<Index/>} />
+      <Route path="/login" element= {<Login/>} />
+
+      <Route element={<PrivateRoutes/>}>
+      <Route path="/todos" element= {<Todos/>} />
+      </Route>
     </Routes>
     </>
   )
